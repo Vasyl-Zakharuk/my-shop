5acc9166b7e094f8137925b86efd681305ac1634
Vasyl Zakharuk <zakharukuv@gmail.com>
2022-03-14 18:46:47 +0200

    refactoring app not work

--- src/components/GoodsItem.jsx
+++ src/components/GoodsItem.jsx
@@ -1,12 +1,10 @@
+import { useContext } from 'react';
+import { ShopContext } from '../context';
+
 function GoodsItem(props) {
-    const {
-        id,
-        name,
-        description,
-        price,
-        image_link,
-        addToBasket = Function.prototype,
-    } = props;
+    const { id, name, description, price, image_link } = props;
+
+    const { addToBasket } = useContext(ShopContext);
 
     return (
         <div className='card'>
